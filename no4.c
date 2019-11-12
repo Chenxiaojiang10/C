2a6eefbd4e0a3668ede918f791c6f61eb34d6aff
10 <346877957@qq.com>
2019-11-12 18:54:18 +0800

    九九乘法表

---
+++ no4.c
@@ -0,0 +1,26 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//
+//int main() {
+//	int a[3];
+//	int b, i, j;
+//	printf("请输入3个整数：\n");
+//	  for (i = 0; i <= 2 ; i++)
+//		scanf("%d", &a[i]);
+//
+//	  for (i = 0; i < 2; i++) {
+//
+//		  for (j = 0; j < 2 - i; j++)
+//
+//			  if (a[j] > a[j + 1]) {
+//
+//				  b = a[j];
+//				  a[j] = a[j + 1];
+//				  a[j + 1] = b;
+//			  }
+//	  }
+//	  for(i = 0; i < 3; i++)
+//		printf("从小到大依次为：%d\n", a[i]);
+//
+//	return 0;
+//}
