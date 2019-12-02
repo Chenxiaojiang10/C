7b780a8470a80fd558b4943ade1fd5a3ab1e14d9
10 <346877957@qq.com>
2019-12-02 22:26:23 +0800

    递归计算n的k次方

---
+++ 7-2.c
@@ -0,0 +1,19 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//
+//int M(int n, int k, int i) {
+//	i++;
+//	if(i >= k) {
+//		return n;
+//	}
+//	return n * M(n, k, i);
+//}
+//int main() {
+//	int a = 0;
+//	int b = 0;
+//	int i = 0;
+//	scanf("%d%d", &a, &b);
+//	int p = M(a,b,i);
+//	printf("%d", p);
+//	return 0;
+//}
