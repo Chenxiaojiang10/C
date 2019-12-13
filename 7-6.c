d176d49335edc1bbfde960169c1fffd6167fbbe6
10 <346877957@qq.com>
2019-12-13 21:26:15 +0800

    新增文件

---
+++ 7-6.c
@@ -0,0 +1,26 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+////非递归求n的阶乘
+//int factorial(int n) {
+//	int f = 1;
+//	for (int i = 1; i < n + 1; i++) {
+//		f = f * i;
+//	}
+//	return f;
+//}
+//
+////递归求n的阶乘
+//int Factorial(int n) {
+//	if (n == 1) { 
+//		return 1;
+//	}
+//	return n * Factorial(n-1);
+//}
+//int main() {
+//	int a = 0;
+//	scanf("%d", &a);
+//	int m = factorial(a);
+//	int n = Factorial(a);
+//	printf("%d\n%d", m,n);
+//	return 0;
+//}
