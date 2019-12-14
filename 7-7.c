50af9b92910ea7ed596ea7787291a0857ac2ce03
10 <346877957@qq.com>
2019-12-14 22:05:43 +0800

    新增文件

---
+++ 7-7.c
@@ -0,0 +1,16 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//
+//void printnum(int n) {
+//	if (n > 9) {
+//		printnum(n / 10);
+//	}
+//	printf("%d  ", n % 10);
+//}
+//int main() {
+//	printf("输入一个数字输出它的组成数字：");
+//	int a = 0;
+//	scanf("%d", &a);
+//	printnum(a);
+//	return 0;
+//}
