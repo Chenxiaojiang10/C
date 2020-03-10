f7b0cde71d01b3d0a553469c0a87a8a366536b23
10 <346877957@qq.com>
2020-03-10 19:50:19 +0800

    新增文件

---
+++ 20200121.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<string.h>
+//int canConstruct(char* ransomNote, char* magazine) {
+//	int n = strlen(ransomNote);
+//	int m = strlen(magazine);
+//	int i,j;
+//	int k = 0;
+//	int arr[20] = { 0 };
+//	for (i = 0; i < n; i++) {
+//		for (j = 0; j < m; j++) {
+//			if (ransomNote[i] == magazine[j] && arr[j] == 0) {
+//				arr[j] = 1;
+//				k++;
+//				break;
+//			}
+//		}
+//	}
+//	if(k==n){ 
+//		return 1; 
+//	}
+//	else {
+//		return 0;
+//	}
+//}
+//
+//int main() {
+//	char ransomNote[] = "abc";
+//	char magazine[] = "abhjjgg";
+//	int a=canConstruct(ransomNote, magazine);
+//	if (a == 1) {
+//		printf("true");
+//	}
+//	else {
+//		printf("false");
+//	}
+//	return 0;
+//}
