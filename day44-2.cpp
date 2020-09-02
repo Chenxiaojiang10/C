fb1dffac1c9680ff99658c2f8f43d0e5052cbe96
10 <346877957@qq.com>
2020-09-02 23:44:04 +0800

    将单词逆序，例如i am a student变成student a am i

---
+++ day44-2.cpp
@@ -0,0 +1,30 @@
+//#include<iostream>
+//#include<string>
+////°ÑI am a student±ä³Éstudent a am I
+//using namespace std;
+//
+//int main() {
+//	string s;
+//	while (getline(cin, s)) {
+//		int cur = s.size() - 1;
+//		int tmp = cur + 1;
+//		while (cur >= 0) {
+//			if (s[cur] == ' ') {
+//				for (int i = cur + 1; i < tmp; i++) {
+//					cout << s[i];
+//				}
+//				cout << ' ';
+//				tmp = cur;
+//			}
+//
+//			cur--;
+//
+//
+//		}
+//		for (int i = 0; i < tmp; i++) {
+//			cout << s[i];
+//		}
+//		s.clear();
+//	}
+//	return 0;
+//}
